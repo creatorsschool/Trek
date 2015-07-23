ddc3e93ade8c3b8c70e931d9c8584f76c5d9969f
Claudia <aclaudia.ricardo@gmail.com>
2015-07-23 15:04:23 +0100

    create task

--- app/actions.rb
+++ app/actions.rb
@@ -1,28 +1,13 @@
 get "/" do
 	@users = User.all
 	@groups = Group.all
+	@tasks = Task.all
 	erb :index
 end
 
+#.....................................User................................................
 get '/user/:id/remove' do
 	User.destroy(params[:id])
-	redirect '/'
-end
-
-get '/group/:id/remove' do
-	Group.destroy(params[:id])
-	redirect '/'
-end
-
-get "/group/new" do
-	erb :"/groups/group_form-create"
-end
-
-post '/group/create' do
-	Group.create({
-		name: params[:name],
-		description: params[:description],
-		})
 	redirect '/'
 end
 
@@ -48,4 +33,37 @@ end
 post '/users/:id/update' do
     User.update(params[:id], first_name: paramas[:first_name], last_name: params[:last_name], email: params[:email])
     redirect '/users/#{user_id}'
+end
+
+#----------------------------Group............................................
+
+get '/group/:id/remove' do
+	Group.destroy(params[:id])
+	redirect '/'
+end
+
+get "/group/new" do
+	erb :"/groups/group_form-create"
+end
+
+post '/group/create' do
+	Group.create({
+		name: params[:name],
+		description: params[:description],
+		})
+	redirect '/'
+end
+
+#..........................Task.........................................
+
+get "/task/new" do
+	erb :"/tasks/task_form-create"
+end
+
+post '/task/create/:user_id' do
+	Task.create({
+		field: params[:field],
+		user_id: params[:user_id]
+		})
+	redirect '/'
 end
