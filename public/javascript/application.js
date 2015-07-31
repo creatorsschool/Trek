dccdb69d6bd2b79c34a1b61e671a0d6d2e901cec
Luis Rego <luisregogouveia@gmail.com>
2015-07-31 15:11:58 +0100

    search bar

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     $('.modal-trigger').leanModal();
-  });
+    $(".button-collapse").sideNav();
+    $(".teste").on("click", function(evt) {
+      evt.preventDefault();
+      $(".search-container").slideToggle();
+    });
+});
