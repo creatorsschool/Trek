d04b4c4807620ac3de49e8e0aeb85777f364e823
Fábio Henriques <fabyohenriques@gmail.com>
2015-08-03 15:17:22 +0100

    sign in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150731172543) do
+ActiveRecord::Schema.define(version: 20150803141022) do
 
   create_table "groups", force: :cascade do |t|
     t.string  "name"
@@ -47,6 +47,12 @@ ActiveRecord::Schema.define(version: 20150731172543) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
+    t.string "encrypted_password", limit: 128
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token",     limit: 128
   end
+
+  add_index "users", ["email"], name: "index_users_on_email"
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
