bf0cc3b75f471d717fcccecd6ae7582ccd8b584f
Ruben <RubenclFerreira>
2015-08-03 16:20:46 +0100

    add first name and last name to sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class UsersController < Clearance::UsersController
   def show
     @user = User.find do |user|
       user.id == params[:id].to_i
@@ -19,5 +19,19 @@ class UsersController < ApplicationController
   def destroy
     User.destroy(params[:id])
     redirect_to dashboard_path
+  end
+
+  def user_from_params
+    first_name = user_params.delete(:first_name)
+    last_name = user_params.delete(:last_name)
+    email = user_params.delete(:email)
+    password = user_params.delete(:password)
+
+    Clearance.configuration.user_model.new(user_params).tap do |user|
+      user.first_name = first_name
+      user.last_name = last_name
+      user.email = email
+      user.password = password
+    end
   end
 end
