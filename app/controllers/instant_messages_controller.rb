effebb2c5d93edd1580690a77f5f5c4a5564e558
Claudia <aclaudia.ricardo@gmail.com>
2015-08-06 12:31:25 +0100

    instant-messenger30%

--- app/controllers/instant_messages_controller.rb
+++ app/controllers/instant_messages_controller.rb
@@ -1,18 +1,24 @@
 class InstantMessagesController < ApplicationController
 
 	def index
-		@messages_sent = current_user.sent_messages
-		@messages_received = current_user.received_messages
+		@user_id_received = params[:user_id]
+		@user_name_received = User.find(params[:user_id])
+		@messages_user_sent = InstantMessage.where(user_id_sent: current_user.id, user_id_received: params[:user_id])
+		@messages_user_received = InstantMessage.where(user_id_sent: params[:user_id], user_id_received: current_user.id )
+		@messages=[@messages_user_sent,@messages_user_received].flatten!
+		#@messages_sent = current_user.sent_messages # todas as mensagens que enviei
+		#@messages_received = current_user.received_messages # todas as mensagens que recebi
 	end
 
 	def create
 		message = InstantMessage.new({
 			text: params[:instant_message][:text],
 			user_id_sent: current_user.id ,
-			user_id_received: 1
+			user_id_received: params[:user_id], 
+			time: DateTime.now
 			})
 		if message.save
-			redirect_to instant_messages_path
+			redirect_to user_instant_messages_path
 		else
 			flash[:error] = "Action failed"
 		end
