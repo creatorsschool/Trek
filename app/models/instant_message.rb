907c0087027d92514a78e6146bdbf37a5212feda
Claudia <aclaudia.ricardo@gmail.com>
2015-08-12 15:03:48 +0100

    modification in time chat

--- app/models/instant_message.rb
+++ app/models/instant_message.rb
@@ -5,11 +5,28 @@ class InstantMessage < ActiveRecord::Base
 
 	def str_time
 		#verifica se esta no fuso horario de verao
-		self.time.between?(DateTime.new(2015,05,29), DateTime.new(2015,10,25)) ? self.time += 1.hour  : self.time
+		if self.time.between?(DateTime.new(2015,05,29), DateTime.new(2015,10,25)) 
+			self.time += 1.hour
+		end 
+
 		#verifica se esta no mesmo dia
-		return self.time.strftime("%H:%M") if DateTime.now.strftime("%d/%m/%Y") == self.time.strftime("%d/%m/%Y")
+		if DateTime.now.strftime("%d/%m/%Y") == self.time.strftime("%d/%m/%Y")
+			hours_message = self.time.hour + self.time.min/60.0 + self.time.sec/3600.0 # passar tudo para horas
+			hours_now = DateTime.now.hour + DateTime.now.min/60.0 + DateTime.now.sec/3600.0
+			dif_minutes = ((hours_message - hours_now)*60).abs
+			if dif_minutes < 1 
+				return "now"
+			elsif dif_minutes.between?(1,60)
+				return "#{dif_minutes.round} min"
+			else
+				return self.time.strftime("%H:%M")
+			end	
+		end
+
 		#verifica se esta na mesmo semana 
-		return self.time.strftime("%a, %H:%M") if self.time.between?(self.time.at_beginning_of_week,self.time.at_end_of_week) 
+		return self.time.strftime("%a, %H:%M") if
+			self.time.between?(self.time.at_beginning_of_week, self.time.at_end_of_week) 
+
 		# se nenhuma das condicoes se verificar
 		return self.time.strftime("%d de %b, %H:%M")
 	end
